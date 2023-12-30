26980b275bb8acca89d4907a1391a48b3c558dfb
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2023-12-30 19:40:50 +0600

    test

---
+++ Binary IPV4 to Decimal.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<math.h>
+#include<stdio.h>
+
+using namespace std;
+
+void convertIntoBinary(int num)
+{
+    int sum = 0;
+    int x = 0;
+    int powerResult = 0;
+    int temp = 0;
+
+    while(num!=0)
+    {
+        int powerResult = pow(2, x);
+
+        int temp = num%2;
+        sum += (temp * powerResult);
+
+        x++;
+        powerResult = 0;
+        num = num/10;
+        temp++;
+    }
+
+    cout<<sum;
+    sum = 0;
+}
+
+int main()
+{
+    int a,b,c,d;
+
+    cout<<"Enter an 32 bit address: ";
+    scanf("%d.%d.%d.%d", &a, &b, &c, &d);
+
+    int arr[] = {a, b, c, d};
+
+    cout<<"\nDecimal address of given address: ";
+    for(int i = 0; i<4; i++)
+    {
+        convertIntoBinary(arr[i]);
+        if(i != 3)
+        {
+            cout<<".";
+        }
+    }
+    cout<<endl;
+    return 0;
+}
